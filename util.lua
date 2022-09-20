5b28ffdc157480c464693feb11b7eb164b0788dd
Caleb Heuer <ch@raiguard.me>
2022-09-19 22:03:33 -0600

    Use new sumneko @enum tag

--- util.lua
+++ util.lua
@@ -110,19 +110,7 @@ end
 
 util.research_queue_updated_event = event.generate_id()
 
--- This is a super ugly way to do an enum type
---- @class ResearchState: integer
---- @class ResearchState.available : ResearchState
---- @class ResearchState.conditionally_available : ResearchState
---- @class ResearchState.not_available : ResearchState
---- @class ResearchState.researched : ResearchState
---- @class ResearchState.disabled : ResearchState
---- @class ResearchState.__index
---- @field available ResearchState.available
---- @field conditionally_available ResearchState.conditionally_available
---- @field not_available ResearchState.not_available
---- @field researched ResearchState.researched
---- @field disabled ResearchState.disabled
+--- @enum ResearchState
 util.research_state = {
   available = 0,
   conditionally_available = 1,
