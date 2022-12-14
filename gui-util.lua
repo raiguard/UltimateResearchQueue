bd3de425e1b6b1abf644efb64a72757dc632ef24
Caleb Heuer <ch@raiguard.me>
2022-12-13 18:00:02 -0700

    Extract several GUI functions into a util file

---
+++ gui-util.lua
@@ -0,0 +1,271 @@
+local math = require("__flib__/math")
+local table = require("__flib__/table")
+
+local constants = require("__UltimateResearchQueue__/constants")
+local util = require("__UltimateResearchQueue__/util")
+
+local gui_util = {}
+
+--- @param effect TechnologyModifier
+function gui_util.effect_button(effect)
+  local sprite, tooltip
+
+  if effect.type == "ammo-damage" then
+    sprite = global.effect_icons[effect.ammo_category]
+    tooltip =
+      { "modifier-description." .. effect.ammo_category .. "-damage-bonus", tostring(effect.modifier * 100) .. "%" }
+  elseif effect.type == "give-item" then
+    sprite = "item/" .. effect.item
+    tooltip = { "", effect.count .. "x  ", game.item_prototypes[effect.item].localised_name }
+  elseif effect.type == "gun-speed" then
+    sprite = global.effect_icons[effect.ammo_category]
+    tooltip = {
+      "modifier-description." .. effect.ammo_category .. "-shooting-speed-bonus",
+      tostring(effect.modifier * 100) .. "%",
+    }
+  elseif effect.type == "nothing" then
+    tooltip = effect.effect_description
+  elseif effect.type == "turret-attack" then
+    sprite = "entity/" .. effect.turret_id
+    tooltip = {
+      "modifier-description." .. effect.turret_id .. "-attack-bonus",
+      tostring(effect.modifier * 100) .. "%",
+    }
+  elseif effect.type == "unlock-recipe" then
+    sprite = "recipe/" .. effect.recipe
+    tooltip = game.recipe_prototypes[effect.recipe].localised_name
+  else
+    sprite = global.effect_icons[effect.type] or ("utility/" .. string.gsub(effect.type, "%-", "_") .. "_modifier_icon")
+    local modifier = effect.modifier
+    --- @type LocalisedString
+    local formatted = tostring(modifier)
+    local format = constants.effect_display_type[effect.type]
+    if format then
+      if format == "float" then
+        formatted = tostring(math.round(modifier, 0.01))
+      elseif format == "float_percent" then
+        formatted = { "format-percent", tostring(math.round(modifier * 100, 0.01)) }
+      elseif format == "signed" or format == "unsigned" then
+        formatted = tostring(math.round(modifier))
+      elseif format == "ticks" then
+        formatted = util.format_time_short(effect.modifier)
+      end
+    end
+    tooltip = { "modifier-description." .. effect.type, formatted }
+  end
+
+  local overlay_constant = constants.overlay_constant[effect.type]
+  --- @type GuiElemDef?
+  local overlay_elem
+  if overlay_constant then
+    overlay_elem =
+      { type = "sprite-button", style = "transparent_slot", sprite = overlay_constant, ignored_by_interaction = true }
+  end
+
+  if DEBUG then
+    if tooltip then
+      tooltip = { "", tooltip, "\n", serpent.block(effect) }
+    else
+      tooltip = serpent.block(effect)
+    end
+  end
+
+  return {
+    type = "sprite-button",
+    style = "transparent_slot",
+    sprite = sprite or "utility/nothing_modifier_icon",
+    number = effect.count,
+    tooltip = tooltip,
+    overlay_elem,
+  }
+end
+
+--- @param name string
+--- @param sprite string
+--- @param tooltip LocalisedString
+--- @param action function
+--- @return GuiElemDef
+function gui_util.frame_action_button(name, sprite, tooltip, action)
+  return {
+    type = "sprite-button",
+    name = name,
+    style = "frame_action_button",
+    tooltip = tooltip,
+    sprite = sprite .. "_white",
+    hovered_sprite = sprite .. "_black",
+    clicked_sprite = sprite .. "_black",
+    handler = { [defines.events.on_gui_click] = action },
+  }
+end
+
+--- @param handler function
+--- @param tech_data TechnologyDataWithLevel
+--- @param is_selected boolean?
+--- @param is_tech_info boolean?
+--- @return GuiElemDef
+function gui_util.technology_slot(handler, tech_data, is_selected, is_tech_info)
+  local tech_data, level = tech_data.data, tech_data.level
+  local properties = gui_util.get_technology_slot_properties(tech_data, is_selected)
+  local progress = util.get_research_progress(tech_data.technology)
+
+  local ingredients = {}
+  local ingredients_len = 0
+  for i, ingredient in pairs(tech_data.technology.research_unit_ingredients) do
+    ingredients_len = i
+    table.insert(ingredients, {
+      type = "sprite",
+      style = "urq_technology_slot_ingredient",
+      sprite = ingredient.type .. "/" .. ingredient.name,
+      ignored_by_interaction = true,
+    })
+  end
+
+  -- TODO: Add remainder to always fill available space
+  local ingredients_spacing = math.clamp((68 - 16) / (ingredients_len - 1) - 16, -15, -5)
+
+  local tooltip = tech_data.technology.localised_name
+  if DEBUG then
+    tooltip = { "", tooltip, "\norder=" .. global.technology_order[tech_data.name] }
+  end
+
+  return {
+    type = "sprite-button",
+    name = tech_data.name,
+    style = properties.style,
+    tooltip = tooltip,
+    ignored_by_interaction = is_tech_info,
+    tags = { research_state = tech_data.research_state, tech_name = tech_data.name, level = level },
+    handler = { [defines.events.on_gui_click] = handler },
+    {
+      type = "flow",
+      style = "urq_technology_slot_sprite_flow",
+      ignored_by_interaction = true,
+      {
+        type = "sprite",
+        style = "urq_technology_slot_sprite",
+        sprite = "technology/" .. tech_data.name,
+      },
+    },
+    (tech_data.is_multilevel or tech_data.base_level > 1) and {
+      type = "label",
+      name = "level_label",
+      style = "urq_technology_slot_level_label_" .. properties.research_state_str,
+      caption = level or tech_data.technology.level,
+      ignored_by_interaction = true,
+    } or {},
+    tech_data.is_multilevel and {
+      type = "label",
+      name = "level_range_label",
+      style = "urq_technology_slot_level_range_label_" .. properties.research_state_str,
+      caption = tech_data.base_level .. " - " .. properties.max_level_str,
+      ignored_by_interaction = true,
+    } or {},
+    {
+      type = "flow",
+      style = "urq_technology_slot_ingredients_flow",
+      style_mods = { horizontal_spacing = ingredients_spacing },
+      children = ingredients,
+      ignored_by_interaction = true,
+    },
+    {
+      type = "label",
+      name = "duration_label",
+      style = "urq_technology_slot_duration_label",
+      ignored_by_interaction = true,
+    },
+    {
+      type = "progressbar",
+      name = "progressbar",
+      style = "urq_technology_slot_progressbar",
+      value = progress,
+      visible = not is_tech_info and progress > 0,
+      ignored_by_interaction = true,
+    },
+  }
+end
+
+--- @param elem LuaGuiElement
+function gui_util.is_double_click(elem)
+  local tags = elem.tags
+  local last_click_tick = tags.last_click_tick or 0
+  local is_double_click = game.ticks_played - last_click_tick < 12
+  if is_double_click then
+    tags.last_click_tick = nil
+  else
+    tags.last_click_tick = game.ticks_played
+  end
+  elem.tags = tags
+  return is_double_click
+end
+
+--- @param element LuaGuiElement
+--- @param parent LuaGuiElement
+--- @param index number
+function gui_util.move_to(element, parent, index)
+  --- @cast index uint
+  local dummy = parent.add({ type = "empty-widget", index = index })
+  parent.swap_children(element.get_index_in_parent(), index)
+  dummy.destroy()
+end
+
+--- @param tech_data TechnologyData
+--- @param is_selected boolean?
+--- @return TechnologySlotProperties
+function gui_util.get_technology_slot_properties(tech_data, is_selected)
+  local research_state_str = table.find(constants.research_state, tech_data.research_state)
+  local max_level_str = tech_data.max_level == math.max_uint and "[img=infinity]" or tostring(tech_data.max_level)
+  local style = "urq_technology_slot_"
+    .. research_state_str
+    .. ((tech_data.is_multilevel or tech_data.base_level > 1) and "_leveled" or "")
+    .. (is_selected and "_selected" or "")
+
+  --- @class TechnologySlotProperties
+  return { max_level_str = max_level_str, research_state_str = research_state_str, style = style }
+end
+
+--- @param button LuaGuiElement
+--- @param tech_data TechnologyDataWithLevel
+--- @param is_selected boolean?
+function gui_util.update_tech_slot(button, tech_data, is_selected)
+  local tech_data, level = tech_data.data, tech_data.level
+  local properties = gui_util.get_technology_slot_properties(tech_data, is_selected)
+  local tags = button.tags
+  local research_state = tech_data.research_state
+  if tags.research_state ~= research_state then
+    button.style = properties.style
+    if research_state == constants.research_state.researched then
+      button.progressbar.visible = false
+      button.progressbar.value = 0
+    end
+    if tech_data.is_multilevel or tech_data.base_level > 1 then
+      button.level_label.style = "urq_technology_slot_level_label_" .. properties.research_state_str
+    end
+    if tech_data.is_multilevel then
+      button.level_range_label.style = "urq_technology_slot_level_range_label_" .. properties.research_state_str
+    end
+    tags.research_state = research_state --[[@as AnyBasic]]
+    button.tags = tags
+  end
+  if tech_data.is_multilevel then
+    local level_label = button.level_label
+    if level_label then
+      level_label.caption = tostring(level or tech_data.technology.level)
+    end
+  end
+  button.duration_label.visible = tech_data.in_queue
+end
+
+--- @param elem LuaGuiElement
+--- @param value boolean
+--- @param sprite_base string
+function gui_util.toggle_frame_action_button(elem, sprite_base, value)
+  if value then
+    elem.style = "flib_selected_frame_action_button"
+    elem.sprite = sprite_base .. "_black"
+  else
+    elem.style = "frame_action_button"
+    elem.sprite = sprite_base .. "_white"
+  end
+end
+
+return gui_util
--- gui.lua
+++ gui.lua
@@ -4,98 +4,13 @@ local math = require("__flib__/math")
 local table = require("__flib__/table")
 
 local constants = require("__UltimateResearchQueue__/constants")
+local gui_util = require("__UltimateResearchQueue__/gui-util")
 local research_queue = require("__UltimateResearchQueue__/research-queue")
 local util = require("__UltimateResearchQueue__/util")
 
 --- @class TechnologyDataWithLevel
 --- @field data TechnologyData
 --- @field level uint
-
--- Utilities
-
---- @param elem LuaGuiElement
---- @param value boolean
---- @param sprite_base string
-local function toggle_frame_action_button(elem, sprite_base, value)
-  if value then
-    elem.style = "flib_selected_frame_action_button"
-    elem.sprite = sprite_base .. "_black"
-  else
-    elem.style = "frame_action_button"
-    elem.sprite = sprite_base .. "_white"
-  end
-end
-
---- @param elem LuaGuiElement
-local function is_double_click(elem)
-  local tags = elem.tags
-  local last_click_tick = tags.last_click_tick or 0
-  local is_double_click = game.ticks_played - last_click_tick < 12
-  if is_double_click then
-    tags.last_click_tick = nil
-  else
-    tags.last_click_tick = game.ticks_played
-  end
-  elem.tags = tags
-  return is_double_click
-end
-
---- @param element LuaGuiElement
---- @param parent LuaGuiElement
---- @param index number
-local function move_to(element, parent, index)
-  --- @cast index uint
-  local dummy = parent.add({ type = "empty-widget", index = index })
-  parent.swap_children(element.get_index_in_parent(), index)
-  dummy.destroy()
-end
-
---- @param tech_data TechnologyData
---- @param is_selected boolean?
---- @return TechnologySlotProperties
-local function get_technology_slot_properties(tech_data, is_selected)
-  local research_state_str = table.find(constants.research_state, tech_data.research_state)
-  local max_level_str = tech_data.max_level == math.max_uint and "[img=infinity]" or tostring(tech_data.max_level)
-  local style = "urq_technology_slot_"
-    .. research_state_str
-    .. ((tech_data.is_multilevel or tech_data.base_level > 1) and "_leveled" or "")
-    .. (is_selected and "_selected" or "")
-
-  --- @class TechnologySlotProperties
-  return { max_level_str = max_level_str, research_state_str = research_state_str, style = style }
-end
-
---- @param button LuaGuiElement
---- @param tech_data TechnologyDataWithLevel
---- @param is_selected boolean?
-local function update_tech_slot(button, tech_data, is_selected)
-  local tech_data, level = tech_data.data, tech_data.level
-  local properties = get_technology_slot_properties(tech_data, is_selected)
-  local tags = button.tags
-  local research_state = tech_data.research_state
-  if tags.research_state ~= research_state then
-    button.style = properties.style
-    if research_state == constants.research_state.researched then
-      button.progressbar.visible = false
-      button.progressbar.value = 0
-    end
-    if tech_data.is_multilevel or tech_data.base_level > 1 then
-      button.level_label.style = "urq_technology_slot_level_label_" .. properties.research_state_str
-    end
-    if tech_data.is_multilevel then
-      button.level_range_label.style = "urq_technology_slot_level_range_label_" .. properties.research_state_str
-    end
-    tags.research_state = research_state --[[@as AnyBasic]]
-    button.tags = tags
-  end
-  if tech_data.is_multilevel then
-    local level_label = button.level_label
-    if level_label then
-      level_label.caption = tostring(level or tech_data.technology.level)
-    end
-  end
-  button.duration_label.visible = tech_data.in_queue
-end
 
 --- @class GuiElems
 --- @field urq_window LuaGuiElement
@@ -128,187 +43,6 @@ end
 
 --- @class GuiMod
 local gui = {}
-
--- Templates
-
---- @param effect TechnologyModifier
-local function effect_button(effect)
-  local sprite, tooltip
-
-  if effect.type == "ammo-damage" then
-    sprite = global.effect_icons[effect.ammo_category]
-    tooltip =
-      { "modifier-description." .. effect.ammo_category .. "-damage-bonus", tostring(effect.modifier * 100) .. "%" }
-  elseif effect.type == "give-item" then
-    sprite = "item/" .. effect.item
-    tooltip = { "", effect.count .. "x  ", game.item_prototypes[effect.item].localised_name }
-  elseif effect.type == "gun-speed" then
-    sprite = global.effect_icons[effect.ammo_category]
-    tooltip = {
-      "modifier-description." .. effect.ammo_category .. "-shooting-speed-bonus",
-      tostring(effect.modifier * 100) .. "%",
-    }
-  elseif effect.type == "nothing" then
-    tooltip = effect.effect_description
-  elseif effect.type == "turret-attack" then
-    sprite = "entity/" .. effect.turret_id
-    tooltip = {
-      "modifier-description." .. effect.turret_id .. "-attack-bonus",
-      tostring(effect.modifier * 100) .. "%",
-    }
-  elseif effect.type == "unlock-recipe" then
-    sprite = "recipe/" .. effect.recipe
-    tooltip = game.recipe_prototypes[effect.recipe].localised_name
-  else
-    sprite = global.effect_icons[effect.type] or ("utility/" .. string.gsub(effect.type, "%-", "_") .. "_modifier_icon")
-    local modifier = effect.modifier
-    --- @type LocalisedString
-    local formatted = tostring(modifier)
-    local format = constants.effect_display_type[effect.type]
-    if format then
-      if format == "float" then
-        formatted = tostring(math.round(modifier, 0.01))
-      elseif format == "float_percent" then
-        formatted = { "format-percent", tostring(math.round(modifier * 100, 0.01)) }
-      elseif format == "signed" or format == "unsigned" then
-        formatted = tostring(math.round(modifier))
-      elseif format == "ticks" then
-        formatted = util.format_time_short(effect.modifier)
-      end
-    end
-    tooltip = { "modifier-description." .. effect.type, formatted }
-  end
-
-  local overlay_constant = constants.overlay_constant[effect.type]
-  --- @type GuiElemDef?
-  local overlay_elem
-  if overlay_constant then
-    overlay_elem =
-      { type = "sprite-button", style = "transparent_slot", sprite = overlay_constant, ignored_by_interaction = true }
-  end
-
-  if DEBUG then
-    if tooltip then
-      tooltip = { "", tooltip, "\n", serpent.block(effect) }
-    else
-      tooltip = serpent.block(effect)
-    end
-  end
-
-  return {
-    type = "sprite-button",
-    style = "transparent_slot",
-    sprite = sprite or "utility/nothing_modifier_icon",
-    number = effect.count,
-    tooltip = tooltip,
-    overlay_elem,
-  }
-end
-
---- @param name string
---- @param sprite string
---- @param tooltip LocalisedString
---- @param action function
---- @return GuiElemDef
-local function frame_action_button(name, sprite, tooltip, action)
-  return {
-    type = "sprite-button",
-    name = name,
-    style = "frame_action_button",
-    tooltip = tooltip,
-    sprite = sprite .. "_white",
-    hovered_sprite = sprite .. "_black",
-    clicked_sprite = sprite .. "_black",
-    handler = { [defines.events.on_gui_click] = action },
-  }
-end
-
---- @param tech_data TechnologyDataWithLevel
---- @param is_selected boolean?
---- @param is_tech_info boolean?
---- @return GuiElemDef
-local function technology_slot(tech_data, is_selected, is_tech_info)
-  local tech_data, level = tech_data.data, tech_data.level
-  local properties = get_technology_slot_properties(tech_data, is_selected)
-  local progress = util.get_research_progress(tech_data.technology)
-
-  local ingredients = {}
-  local ingredients_len = 0
-  for i, ingredient in pairs(tech_data.technology.research_unit_ingredients) do
-    ingredients_len = i
-    table.insert(ingredients, {
-      type = "sprite",
-      style = "urq_technology_slot_ingredient",
-      sprite = ingredient.type .. "/" .. ingredient.name,
-      ignored_by_interaction = true,
-    })
-  end
-
-  -- TODO: Add remainder to always fill available space
-  local ingredients_spacing = math.clamp((68 - 16) / (ingredients_len - 1) - 16, -15, -5)
-
-  local tooltip = tech_data.technology.localised_name
-  if DEBUG then
-    tooltip = { "", tooltip, "\norder=" .. global.technology_order[tech_data.name] }
-  end
-
-  return {
-    type = "sprite-button",
-    name = tech_data.name,
-    style = properties.style,
-    tooltip = tooltip,
-    ignored_by_interaction = is_tech_info,
-    tags = { research_state = tech_data.research_state, tech_name = tech_data.name, level = level },
-    handler = { [defines.events.on_gui_click] = gui.on_tech_slot_click },
-    {
-      type = "flow",
-      style = "urq_technology_slot_sprite_flow",
-      ignored_by_interaction = true,
-      {
-        type = "sprite",
-        style = "urq_technology_slot_sprite",
-        sprite = "technology/" .. tech_data.name,
-      },
-    },
-    (tech_data.is_multilevel or tech_data.base_level > 1) and {
-      type = "label",
-      name = "level_label",
-      style = "urq_technology_slot_level_label_" .. properties.research_state_str,
-      caption = level or tech_data.technology.level,
-      ignored_by_interaction = true,
-    } or {},
-    tech_data.is_multilevel and {
-      type = "label",
-      name = "level_range_label",
-      style = "urq_technology_slot_level_range_label_" .. properties.research_state_str,
-      caption = tech_data.base_level .. " - " .. properties.max_level_str,
-      ignored_by_interaction = true,
-    } or {},
-    {
-      type = "flow",
-      style = "urq_technology_slot_ingredients_flow",
-      style_mods = { horizontal_spacing = ingredients_spacing },
-      children = ingredients,
-      ignored_by_interaction = true,
-    },
-    {
-      type = "label",
-      name = "duration_label",
-      style = "urq_technology_slot_duration_label",
-      ignored_by_interaction = true,
-    },
-    {
-      type = "progressbar",
-      name = "progressbar",
-      style = "urq_technology_slot_progressbar",
-      value = progress,
-      visible = not is_tech_info and progress > 0,
-      ignored_by_interaction = true,
-    },
-  }
-end
-
--- Methods
 
 --- @param self Gui
 --- @param e EventData.on_gui_click
@@ -417,7 +151,7 @@ function gui.on_tech_slot_click(self, e)
     gui.schedule_update(self.force_table)
     return
   end
-  if is_double_click(e.element) then
+  if gui_util.is_double_click(e.element) then
     gui.start_research(self, wrapped_data)
     return
   end
@@ -487,7 +221,7 @@ function gui.select_tech(self, tech_data)
   local main_slot_frame = self.elems.tech_info_main_slot_frame
   main_slot_frame.clear() -- The best thing to do is clear it, otherwise we'd need to diff all the sub-elements
   if tech_data then
-    flib_gui.add(main_slot_frame, technology_slot(tech_data, nil, true))
+    flib_gui.add(main_slot_frame, gui_util.technology_slot(gui.on_tech_slot_click, tech_data, nil, true))
   end
   -- Name and description
   self.elems.tech_info_name_label.caption = technology.localised_name
@@ -511,7 +245,7 @@ function gui.select_tech(self, tech_data)
   -- Effects
   local effects_table = self.elems.tech_info_effects_table
   effects_table.clear()
-  flib_gui.add(effects_table, table.map(technology.effects, effect_button))
+  flib_gui.add(effects_table, table.map(technology.effects, gui_util.effect_button))
   -- Footer
   gui.update_tech_info_footer(self)
 end
@@ -595,7 +329,7 @@ end
 --- @param self Gui
 function gui.toggle_pinned(self)
   self.state.pinned = not self.state.pinned
-  toggle_frame_action_button(self.elems.pin_button, "flib_pin", self.state.pinned)
+  gui_util.toggle_frame_action_button(self.elems.pin_button, "flib_pin", self.state.pinned)
   if self.state.pinned then
     self.player.opened = nil
     self.elems.search_button.tooltip = { "gui.search" }
@@ -611,7 +345,7 @@ end
 --- @param self Gui
 function gui.toggle_search(self)
   self.state.search_open = not self.state.search_open
-  toggle_frame_action_button(self.elems.search_button, "utility/search", self.state.search_open)
+  gui_util.toggle_frame_action_button(self.elems.search_button, "utility/search", self.state.search_open)
   self.elems.search_textfield.visible = self.state.search_open
   if self.state.search_open then
     self.elems.search_textfield.focus()
@@ -717,10 +451,10 @@ function gui.update_queue(self)
     local tech_data = node.data
     -- FIXME: Selected styles
     if button then
-      move_to(button, queue_table, i)
-      update_tech_slot(button, { data = tech_data, level = node.level })
+      gui_util.move_to(button, queue_table, i)
+      gui_util.update_tech_slot(button, { data = tech_data, level = node.level })
     else
-      local button_template = technology_slot({ data = tech_data, level = node.level })
+      local button_template = gui_util.technology_slot(gui.on_tech_slot_click, { data = tech_data, level = node.level })
       button_template.index = i
       flib_gui.add(queue_table, button_template)
     end
@@ -796,10 +530,10 @@ function gui.update_tech_list(self)
         -- FIXME: Selected styles
         local button = techs_table[tech_data.name]
         if button then
-          move_to(button, techs_table, i)
-          update_tech_slot(button, { data = tech_data })
+          gui_util.move_to(button, techs_table, i)
+          gui_util.update_tech_slot(button, { data = tech_data })
         else
-          local button_template = technology_slot({ data = tech_data })
+          local button_template = gui_util.technology_slot(gui.on_tech_slot_click, { data = tech_data })
           button_template.index = i
           flib_gui.add(techs_table, { button_template })
         end
@@ -858,9 +592,14 @@ function gui.new(player)
           clear_and_focus_on_right_click = true,
           handler = { [defines.events.on_gui_text_changed] = gui.update_search_query },
         },
-        frame_action_button("search_button", "utility/search", { "gui.urq-search-instruction" }, gui.toggle_search),
-        frame_action_button("pin_button", "flib_pin", { "gui.flib-keep-open" }, gui.toggle_pinned),
-        frame_action_button("close_button", "utility/close", { "gui.close-instruction" }, gui.hide),
+        gui_util.frame_action_button(
+          "search_button",
+          "utility/search",
+          { "gui.urq-search-instruction" },
+          gui.toggle_search
+        ),
+        gui_util.frame_action_button("pin_button", "flib_pin", { "gui.flib-keep-open" }, gui.toggle_pinned),
+        gui_util.frame_action_button("close_button", "utility/close", { "gui.close-instruction" }, gui.hide),
       },
       {
         type = "flow",
